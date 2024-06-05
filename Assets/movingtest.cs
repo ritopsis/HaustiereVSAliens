84be1c7111bbb0d9e954d6907d2c96b0edb5b066
Numan52 <numan.demir@gmx.at>
2024-06-05 15:12:33 +0200

    basic UI

--- Assets/movingtest.cs
+++ Assets/movingtest.cs
@@ -6,5 +6,17 @@ using UnityEngine;
 
 public class movingtest : NetworkBehaviour
 {
+    private void Update()
+    {
+        if (!IsOwner) return;
+        Vector3 moveDirection = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.W)) moveDirection.y = +1f;
+        if (Input.GetKey(KeyCode.S)) moveDirection.y = -1f;
+        if (Input.GetKey(KeyCode.A)) moveDirection.x = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDirection.x = +1f;
+
+        float moveSpeed = 3f;
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
 }
 
