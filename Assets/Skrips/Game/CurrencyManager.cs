55a564cd7504621e760bf0a8b17cd944b4bf0395
Numan52 <numan.demir@gmx.at>
2024-06-23 13:01:03 +0200

    generating currency in intervals

--- Assets/Skrips/Game/CurrencyManager.cs
+++ Assets/Skrips/Game/CurrencyManager.cs
@@ -2,13 +2,11 @@ using UnityEngine;
 using TMPro;
 using Unity.Netcode;
 using System;
+using System.Collections;
 
 public class CurrencyManager : NetworkBehaviour
 {
     public static CurrencyManager instance;
-
-    //private NetworkVariable<int> petCurrency = new NetworkVariable<int>();
-    //private NetworkVariable<int> alienCurrency = new NetworkVariable<int>();
 
     public int petCurrency = 50;
     public int alienCurrency = 50;
@@ -29,49 +27,115 @@ public class CurrencyManager : NetworkBehaviour
         }
     }
 
+    
+
+    
+    private IEnumerator AddCurrencyEvery10Seconds()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(10f); // Wait for 10 seconds
+
+            Debug.Log("Added 5 currency");
+            AddPetCurrency(5);
+            AddAlienCurrency(5);
+
+            UpdateCurrencyClientRpc(petCurrency, alienCurrency);
+        }
+    }
+
+
+
     void Start()
     {
         petCurrencyText.text = petCurrency.ToString();
         alienCurrencyText.text = alienCurrency.ToString();
 
+        
+        StartCoroutine(AddCurrencyEvery10Seconds());
+        
     }
 
-    
+    [ServerRpc(RequireOwnership = false)]
+    private void AddPetCurrencyServerRpc(int amount)
+    {
+        petCurrency += amount;
+        UpdateCurrencyUI();
+        OnCurrencyChanged();
+        Debug.Log("Add pet currency");
+
+        // Synchronize with clients
+        UpdateCurrencyClientRpc(petCurrency, alienCurrency);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void AddAlienCurrencyServerRpc(int amount)
+    {
+        alienCurrency += amount;
+        UpdateCurrencyUI();
+        OnCurrencyChanged();
+
+        // Synchronize with clients
+        UpdateCurrencyClientRpc(petCurrency, alienCurrency);
+    }
+
+    [ClientRpc]
+    private void UpdateCurrencyClientRpc(int newPetCurrency, int newAlienCurrency)
+    {
+        petCurrency = newPetCurrency;
+        alienCurrency = newAlienCurrency;
+        UpdateCurrencyUI();
+    }
+
     public void AddPetCurrency(int amount)
     {
-       
-        //if (IsServer)
-        //{
-            petCurrency+= amount;
-            UpdateCurrencyUI();
-            OnCurrencyChanged();
-            Debug.Log("Add pet currency");
-        //}
+        if (IsServer)
+        {
+            AddPetCurrencyServerRpc(amount);
+            Debug.Log("add pet currency");
+        }
     }
 
     public void AddAlienCurrency(int amount)
     {
-        
-        //if (IsServer)
-        //{
-            alienCurrency += amount;
-            UpdateCurrencyUI();
-            OnCurrencyChanged();
-        //}
+        if (IsServer)
+        {
+            AddAlienCurrencyServerRpc(amount);
+        }
     }
 
-    public void SubtractPetCurrency(int amount) 
+    public void SubtractPetCurrency(int amount)
     {
-        petCurrency -= amount; 
-        UpdateCurrencyUI();
-        OnCurrencyChanged();
+        SubtractPetCurrencyServerRpc(amount);
     }
+
 
     public void SubtractAlienCurrency(int amount)
+    {
+        SubtractAlienCurrencyServerRpc(amount);
+    }
+
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SubtractPetCurrencyServerRpc(int amount)
+    {
+        petCurrency -= amount;
+        UpdateCurrencyUI();
+        OnCurrencyChanged();
+
+        // Synchronize with clients
+        UpdateCurrencyClientRpc(petCurrency, alienCurrency);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SubtractAlienCurrencyServerRpc(int amount)
     {
         alienCurrency -= amount;
         UpdateCurrencyUI();
         OnCurrencyChanged();
+
+        // Synchronize with clients
+        UpdateCurrencyClientRpc(petCurrency, alienCurrency);
     }
 
     public int GetPetCurrency()
@@ -83,7 +147,6 @@ public class CurrencyManager : NetworkBehaviour
     {
         return alienCurrency;
     }
-
 
     private void UpdateCurrencyUI()
     {
